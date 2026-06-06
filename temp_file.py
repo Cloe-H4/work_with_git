e340d9ff8600c3b6044e8ee7ad9131f0614cbaf6
Nchedochukwu <nchedo017@gmail.com>
2026-06-06 19:05:20 +0100

    Removed loading

--- temp_file.py
+++ temp_file.py
@@ -13,16 +13,6 @@ logging.basicConfig(
 )
 logging.info("========== WAREHOUSE PIPELINE STARTED ==========")
 
-# Load database credentials
-load_dotenv()
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-engine = create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
-logging.info("Database engine created successfully")
-
 # Function to execute SQL files
 def run_sql_file(filepath):
     logging.info(f"Running {filepath}")
